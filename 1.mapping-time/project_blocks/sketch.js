8418d9f265de0503384a6b8a262a874d397eb261
Neil Oliver <admin@Neils-MacBook-Pro.local>
2019-09-25 08:59:19 -0400

    blocks update

--- 1.mapping-time/project_blocks/sketch.js
+++ 1.mapping-time/project_blocks/sketch.js
@@ -23,23 +23,26 @@ function draw() {
   var now = clock()
 
   var secsHeight = (h-(spacing*3))/3
-  var minsWidth = ((w-(spacing*3))/3)*2
-  var hourWidth = (w-(spacing*3))/3
+  var hourWidth = (w-(spacing*3))*now.progress.day
+  var minsWidth = (w-(spacing*3))*(1-now.progress.day)
 
   var secsWidth = (w-(spacing*2)) * now.progress.sec
   var minsHeight = maxHeight * now.progress.min
   var hourHeight = maxHeight * now.progress.hour
 
   fill(167, 217, 217)
+  // second box
   rect(spacing, spacing, w-(spacing*2), secsHeight)
-  rect(spacing, maxHeight + (spacing*2), minsWidth, maxHeight*2)
-  rect(minsWidth+(spacing*2), maxHeight + (spacing*2), hourWidth, maxHeight*2)
+  // hour box 
+  rect(spacing, maxHeight + (spacing*2), hourWidth, maxHeight*2)
+  // hour box
+  rect(hourWidth+(spacing*2), maxHeight + (spacing*2), minsWidth, maxHeight*2)
 
   fill(colorForProgress(now.progress.sec,['#1B8CA6','#A7D9D9']))
   rect(spacing, spacing, secsWidth, secsHeight)
-  fill(colorForProgress(now.progress.min, ['#0F698C','#A7D9D9']))
-  rect(spacing, maxHeight + (spacing*2), minsWidth, minsHeight*2)
-  fill(colorForProgress(now.progress.hour,['#022840','#A7D9D9']))
-  rect(minsWidth+(spacing*2), maxHeight + (spacing*2) + ((maxHeight*2)-hourHeight*2), hourWidth, hourHeight*2)
+  fill(colorForProgress(now.progress.hour, ['#0F698C','#A7D9D9']))
+  rect(spacing, maxHeight + (spacing*2), hourWidth, hourHeight*2)
+  fill(colorForProgress(now.progress.min,['#022840','#A7D9D9']))
+  rect(hourWidth+(spacing*2), maxHeight + (spacing*2) + ((maxHeight*2)-minsHeight*2), minsWidth, minsHeight*2)
 
 }
